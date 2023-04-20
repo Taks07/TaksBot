572f3d5fb928b93d98b75de99d2c32918b7bd26d
Yutaka <yutakaoishi7@gmail.com>
2023-04-20 17:00:51 +1200

    Discord bot token retrieved from .env file to create bot

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -1,7 +1,14 @@
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDABuilder;
 public class DiscordBot {
     public static void main(String[] args) {
+        // Get Discord bot token from .env file
+        Dotenv dotenv = Dotenv.load();
+        final String TOKEN = dotenv.get("TOKEN");
 
+        // Create bot
+        JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN);
+        jdaBuilder.build();
     }
 
 }
