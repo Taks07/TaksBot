7cef39414c360ea260a986ea18d248e60d971236
Yutaka <yutakaoishi7@gmail.com>
2023-04-20 17:45:54 +1200

    DadJokeEventListener uses regex to check if message has 'im ___'. If it does, outputs message to console

--- src/main/java/events/DadJokeEventListener.java
+++ src/main/java/events/DadJokeEventListener.java
@@ -3,14 +3,23 @@ package events;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class DadJokeEventListener extends ListenerAdapter {
+
+    static Pattern pattern = Pattern.compile("i'?m .+", Pattern.CASE_INSENSITIVE);
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         super.onMessageReceived(event);
 
-        // Get contents of message and print
+        // Get contents of message and check if it matches regex pattern
         String messageContent = event.getMessage().getContentDisplay();
-        System.out.println(messageContent);
+        Matcher matcher = pattern.matcher(messageContent);
+
+        if (matcher.find()) {
+            System.out.println(messageContent);
+        }
     }
 }
