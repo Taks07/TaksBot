5ed44c5f196b682a6583789fb0382d7d8336afc2
Yutaka <yutakaoishi7@gmail.com>
2023-05-05 14:00:03 +1200

    Renamed isBotMessage to isOwnMessage to make clearer. Bot now checks if the delete button is on the message before deleting it.

--- src/main/java/events/DeleteBotMessageListener.java
+++ src/main/java/events/DeleteBotMessageListener.java
@@ -1,10 +1,14 @@
 package events;
 
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.RestAction;
 import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
+
+import java.util.List;
 
 public class DeleteBotMessageListener extends ListenerAdapter {
     static Emoji deleteEmoji = Emoji.fromUnicode("🗑️");
@@ -13,13 +17,12 @@ public class DeleteBotMessageListener extends ListenerAdapter {
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
 
         event.getChannel().retrieveMessageById(event.getMessageId()).queue(message -> {
-            // Check if reaction is on own message, is not own reaction, is the deleteEmoji, and was reacted by user being replied to
-            boolean isBotMessage = message.getAuthor().equals(event.getJDA().getSelfUser());
-            boolean isNotOwnReaction = !event.getUser().equals(event.getJDA().getSelfUser());
+            // Check if reaction is on own message, is the deleteEmoji, and was reacted by user being replied to
+            boolean isOwnMessage = message.getAuthor().equals(event.getJDA().getSelfUser());
             boolean isDeleteEmoji = event.getReaction().getEmoji().equals(deleteEmoji);
             boolean isReferencedUser;
 
-            if (isBotMessage & isNotOwnReaction & isDeleteEmoji) {
+            if (isOwnMessage & isDeleteEmoji & hasDeleteButton(message)) {
                 isReferencedUser = message.getMessageReference().getMessage().getAuthor().equals(event.getUser());
 
                 if (isReferencedUser) {
@@ -32,4 +35,16 @@ public class DeleteBotMessageListener extends ListenerAdapter {
         return message.flatMap(botMessage -> botMessage.addReaction(deleteEmoji));
     }
 
+    private static boolean hasDeleteButton(Message message) {
+        List<MessageReaction> reactions = message.getReactions();
+
+        // Check if reaction is equal to deleteEmoji and was reacted by self (i.e. the bot)
+        for (MessageReaction reaction : reactions) {
+            if (reaction.getEmoji().equals(deleteEmoji) & reaction.isSelf()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
